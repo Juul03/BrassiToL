1bb8d5a4082509dbf8cd2c72f6dffcfd85afa24f
Juul03 <118166691+Juul03@users.noreply.github.com>
2023-12-19 13:01:27 +0100

    basic_working_observable_tree

--- src/components/test.svelte
+++ src/components/test.svelte
@@ -52,10 +52,14 @@
 
   onMount(() => {
     const svg = createPhylogeneticTree(parsedData);
-    document.querySelector("#phyloTree").appendChild(svg.node());
+    const container = document.querySelector('#phyloTree');
+
+    if (container) {
+      container.appendChild(svg);
+    } else {
+      console.error("Container not found");
+    }
   });
-
-
 
   const createPhylogeneticTree = (data) => {
     const root = d3
@@ -66,46 +70,189 @@
           a.value - b.value || d3.ascending(a.data.length, b.data.length)
       );
 
-    const cluster = d3.cluster().size([2 * Math.PI, innerRadius]);
-
     cluster(root);
+    setRadius(root, (root.data.length = 0), innerRadius / maxLength(root));
+    setColor(root);
 
     const svg = d3
       .create("svg")
       .attr("viewBox", [-outerRadius, -outerRadius, width, width])
       .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .style("display", "block")
-      .style("margin", "auto")
-      .style("width", "100%")
-      .style("height", "auto");
+      .attr("font-size", 10);
 
-    svg
+    svg.append("g").call(legend);
+
+    svg.append("style").text(`
+
+.link--active {
+  stroke: #000 !important;
+  stroke-width: 1.5px;
+}
+
+.link-extension--active {
+  stroke-opacity: .6;
+}
+
+.label--active {
+  font-weight: bold;
+}
+
+`);
+
+    const linkExtension = svg
       .append("g")
       .attr("fill", "none")
       .attr("stroke", "#000")
-      .attr("stroke-opacity", 0.4)
-      .selectAll("line")
+      .attr("stroke-opacity", 0.25)
+      .selectAll("path")
+      .data(root.links().filter((d) => !d.target.children))
+      .join("path")
+      .each(function (d) {
+        d.target.linkExtensionNode = this;
+      })
+      .attr("d", linkExtensionConstant);
+
+    const link = svg
+      .append("g")
+      .attr("fill", "none")
+      .attr("stroke", "#000")
+      .selectAll("path")
       .data(root.links())
-      .join("line")
-      .attr("x1", (d) => d.source.y * Math.cos(d.source.x - Math.PI / 2))
-      .attr("y1", (d) => d.source.y * Math.sin(d.source.x - Math.PI / 2))
-      .attr("x2", (d) => d.target.y * Math.cos(d.target.x - Math.PI / 2))
-      .attr("y2", (d) => d.target.y * Math.sin(d.target.x - Math.PI / 2));
+      .join("path")
+      .each(function (d) {
+        d.target.linkNode = this;
+      })
+      .attr("d", linkConstant)
+      .attr("stroke", (d) => d.target.color);
 
     svg
       .append("g")
-      .selectAll("circle")
-      .data(root.descendants())
-      .join("circle")
+      .selectAll("text")
+      .data(root.leaves())
+      .join("text")
+      .attr("dy", ".31em")
       .attr(
         "transform",
-        (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`
+        (d) =>
+          `rotate(${d.x - 90}) translate(${innerRadius + 4},0)${
+            d.x < 180 ? "" : " rotate(180)"
+          }`
       )
-      .attr("fill", (d) => (d.children ? "#555" : "#999"))
-      .attr("r", 2);
+      .attr("text-anchor", (d) => (d.x < 180 ? "start" : "end"))
+      .text((d) => d.data.name.replace(/_/g, " "))
+      .on("mouseover", mouseovered(true))
+      .on("mouseout", mouseovered(false));
 
-    return svg;
+    function update(checked) {
+      const t = d3.transition().duration(750);
+      linkExtension
+        .transition(t)
+        .attr("d", checked ? linkExtensionVariable : linkExtensionConstant);
+      link.transition(t).attr("d", checked ? linkVariable : linkConstant);
+    }
+
+    function mouseovered(active) {
+      return function (event, d) {
+        d3.select(this).classed("label--active", active);
+        d3.select(d.linkExtensionNode)
+          .classed("link-extension--active", active)
+          .raise();
+        do d3.select(d.linkNode).classed("link--active", active).raise();
+        while ((d = d.parent));
+      };
+    }
+
+    return Object.assign(svg.node(), { update });
+  };
+
+  // Chart functions
+  let cluster = d3
+    .cluster()
+    .size([360, innerRadius])
+    .separation((a, b) => 1);
+
+  // Set the radius of each node by recursively summing and scaling the distance from the root.
+  let setRadius = (d, y0, k) => {
+    d.radius = (y0 += d.data.length) * k;
+    if (d.children) d.children.forEach((d) => setRadius(d, y0, k));
+  };
+
+  // Compute the maximum cumulative length of any node in the tree.
+  let maxLength = (d) => {
+    return d.data.length + (d.children ? d3.max(d.children, maxLength) : 0);
+  };
+
+  // Set the color of each node by recursively inheriting.
+  let setColor = (d) => {
+    var name = d.data.name;
+    d.color =
+      color.domain().indexOf(name) >= 0
+        ? color(name)
+        : d.parent
+          ? d.parent.color
+          : null;
+    if (d.children) d.children.forEach(setColor);
+  };
+
+  let color = d3
+    .scaleOrdinal()
+    .domain(["Bacteria", "Eukaryota", "Archaea"])
+    .range(d3.schemeCategory10);
+
+  let legend = (svg) => {
+    const g = svg
+      .selectAll("g")
+      .data(color.domain())
+      .join("g")
+      .attr(
+        "transform",
+        (d, i) => `translate(${-outerRadius},${-outerRadius + i * 20})`
+      );
+
+    g.append("rect").attr("width", 18).attr("height", 18).attr("fill", color);
+
+    g.append("text")
+      .attr("x", 24)
+      .attr("y", 9)
+      .attr("dy", "0.35em")
+      .text((d) => d);
+  };
+
+  let linkExtensionConstant = (d) => {
+    return linkStep(d.target.x, d.target.y, d.target.x, innerRadius);
+  };
+
+  let linkStep = (startAngle, startRadius, endAngle, endRadius) => {
+    const c0 = Math.cos((startAngle = ((startAngle - 90) / 180) * Math.PI));
+    const s0 = Math.sin(startAngle);
+    const c1 = Math.cos((endAngle = ((endAngle - 90) / 180) * Math.PI));
+    const s1 = Math.sin(endAngle);
+    return (
+      "M" +
+      startRadius * c0 +
+      "," +
+      startRadius * s0 +
+      (endAngle === startAngle
+        ? ""
+        : "A" +
+          startRadius +
+          "," +
+          startRadius +
+          " 0 0 " +
+          (endAngle > startAngle ? 1 : 0) +
+          " " +
+          startRadius * c1 +
+          "," +
+          startRadius * s1) +
+      "L" +
+      endRadius * c1 +
+      "," +
+      endRadius * s1
+    );
+  };
+
+  let linkConstant = (d) => {
+    return linkStep(d.source.x, d.source.y, d.target.x, d.target.y);
   };
 </script>
 
