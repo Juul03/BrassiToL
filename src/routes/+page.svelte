6fa6ef83aa7e75c23fa146c564963180c5767882
Juul03 <118166691+Juul03@users.noreply.github.com>
2023-12-19 14:28:43 +0100

    removed_unused_files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,7 @@
 <script>
-	import Treemapj from '../components/test.svelte';
-	// import TreemapEx from '../components/exampleTree.svelte';
-	// import Treemap100 from '../components/anotherTree.svelte';
-	// import CircularTree from '../components/circularTree.svelte';
+	import Treemapj from '../components/phylogeneticTree.svelte';
 </script>
 
 <h1>Phylogentic Tree Brassicaceae</h1>
 
 <Treemapj/>
-<!-- <TreemapEx/> -->
