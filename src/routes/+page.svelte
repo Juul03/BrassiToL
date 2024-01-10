4c9a096384cd09ee79d72fae2431f5477f0cad93
DonnaBaijens <donna.baijens@hva.nl>
2024-01-10 15:15:50 +0100

    remove_popup_from_main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
 	import Treemapj from '../components/PhylogeneticTree.svelte';
-    import Popup from '../components/popup.svelte';
+    
 </script>
 
 <h1>Phylogentic Tree Brassicaceae</h1>
 
 <Treemapj/>
-<Popup />
+
   
