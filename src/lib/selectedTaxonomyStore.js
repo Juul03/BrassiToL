003ab86d6823b7b8b786ff6a50daebee8f235cb5
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-01-13 16:19:20 +0100

    changed_store_element_array

--- src/lib/selectedTaxonomyStore.js
+++ src/lib/selectedTaxonomyStore.js
@@ -1,9 +1,14 @@
 import { writable } from 'svelte/store';
 
-// Initialize the writable store with an empty object
-export const selectedTaxonomyStore = writable({});
+// Initialize the store with an empty array
+export const selectedTaxonomyStore = writable([]);
 
-// Update the value of the store whenever the selection changes
-export function updateSelectedTaxonomy(taxonomy) {
-  selectedTaxonomyStore.set(taxonomy);
-}
+// Function to update the store
+export const updateSelectedTaxonomy = (taxonomyArray, taxonomyType) => {
+  selectedTaxonomyStore.update((selectedTaxonomy) => {
+    return {
+      ...selectedTaxonomy,
+      [taxonomyType]: taxonomyArray,
+    };
+  });
+};
