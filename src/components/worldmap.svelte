1e714acfc92005b985e6fb886436048aa8cacf6a
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-01-17 11:04:33 +0100

    color_country_by_selection

--- src/components/worldmap.svelte
+++ src/components/worldmap.svelte
@@ -1,166 +1,164 @@
 <script>
-    import { redirect } from "@sveltejs/kit";
-    import * as d3 from "d3";
-    import { onMount } from "svelte";
-    import { selectedTaxonomyStore } from "$lib/selectedTaxonomyStore";
+  import { redirect } from "@sveltejs/kit";
+  import * as d3 from "d3";
+  import { onMount } from "svelte";
+  import { selectedTaxonomyStore } from "$lib/selectedTaxonomyStore";
 
-    let worldmapgeojson = {};
-    let selectedTaxonomy = {};
-    let allSpeciesData = {};
-    let countryCodeToNamejson = {};
+  let worldmapgeojson = {};
+  let selectedTaxonomy = {};
+  let allSpeciesData = {};
+  let countryCodeToNamejson = {};
 
-    async function fetchgeoJSONData(url) {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            worldmapgeojson = data;
-            console.log(worldmapgeojson);
-        } else {
-            console.error("Failed to fetch the data");
-        }
+  async function fetchgeoJSONData(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      worldmapgeojson = data;
+      console.log(worldmapgeojson);
+    } else {
+      console.error("Failed to fetch the data");
+    }
+  }
+
+  async function fetchData(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      allSpeciesData = data;
+      console.log(allSpeciesData);
+    } else {
+      console.error("Failed to fetch the data");
+    }
+  }
+
+  async function fetchCodeData(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      countryCodeToNamejson = data;
+      console.log(countryCodeToNamejson);
+    } else {
+      console.error("Failed to fetch the data");
+    }
+  }
+
+  onMount(async () => {
+    await fetchgeoJSONData("data/worldmapl2.geojson");
+    await fetchData("data/metadataBrassiToL.json");
+    await fetchCodeData("data/countryCodeToName.json");
+
+    // matchSpecieWithCountry("Arabis scabra", allSpeciesData);
+    // selectedTaxonomy.binaryCombination.forEach((specie) => {
+    //     matchSpecieWithCountry(specie, allSpeciesData);
+    // });
+
+    let projection = d3.geoMercator().fitSize([1000, 600], worldmapgeojson);
+
+    let geoGenerator = d3.geoPath().projection(projection);
+
+    function createMap(worldmapgeojson) {
+      let svg = d3
+        .select("svg")
+        .attr("width", "1050px")
+        .attr("height", "650px")
+        .style("background-color", "blue")
+        .attr("transform", "translate(200, 20)");
+      let u = d3
+        .select("#content g.map")
+        .selectAll("path")
+        .data(worldmapgeojson.features);
+
+      u.enter()
+        .append("path")
+        .attr("d", geoGenerator)
+        .style("fill", "green")
+        .style("stroke", "white")
+        .style("stroke-width", "0.5px");
     }
 
-    async function fetchData(url) {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            allSpeciesData = data;
-            console.log(allSpeciesData);
-        } else {
-            console.error("Failed to fetch the data");
-        }
-    }
+    createMap(worldmapgeojson);
 
-    async function fetchCodeData(url) {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            countryCodeToNamejson = data;
-            console.log(countryCodeToNamejson);
-        } else {
-            console.error("Failed to fetch the data");
-        }
-    }
+    const unsubscribe = selectedTaxonomyStore.subscribe((value) => {
+        selectedTaxonomy = value;
+        console.log("selected", selectedTaxonomy);
 
-    onMount(async () => {
-        await fetchgeoJSONData("data/worldmapl2.geojson");
-        await fetchData("data/metadataBrassiToL.json");
-        await fetchCodeData("data/countryCodeToName.json");
+        let countryCodesArray = selectedTaxonomy.binaryCombination.map(
+            (specie) => {
+                return matchSpecieWithCountryCode(specie, allSpeciesData);
+            }
+        );
 
-        // matchSpecieWithCountry("Arabis scabra", allSpeciesData);
-        // selectedTaxonomy.binaryCombination.forEach((specie) => {
-        //     matchSpecieWithCountry(specie, allSpeciesData);
-        // });
-
-        let projection = d3.geoMercator().fitSize([1000, 600], worldmapgeojson);
-
-        let geoGenerator = d3.geoPath().projection(projection);
-
-        function createMap(worldmapgeojson) {
-            let svg = d3
-                .select("svg")
-                .attr("width", "1050px")
-                .attr("height", "650px")
-                .style("background-color", "blue")
-                .attr("transform", "translate(200, 20)");
-            let u = d3
-                .select("#content g.map")
-                .selectAll("path")
-                .data(worldmapgeojson.features);
-
-            u.enter()
-                .append("path")
-                .attr("d", geoGenerator)
-                .style("fill", "green")
-                .style("stroke", "white")
-                .style("stroke-width", "0.5px");
-        }
-
-        createMap(worldmapgeojson);
-
-        const unsubscribe = selectedTaxonomyStore.subscribe((value) => {
-            selectedTaxonomy = value;
-            console.log("selected", selectedTaxonomy);
-
-            let countryCodesArray = selectedTaxonomy.binaryCombination.map(
-                (specie) => {
-                    return matchSpecieWithCountryCode(specie, allSpeciesData);
-                },
-            );
-
-            let countryNamesArray = countryCodesArray.map((country) => {
-                return matchCountryCodeWithCountryName(country, countryCodeToNamejson
-                );
-            });
-
-            console.log("worst", countryNamesArray);
-
-            // Call the colorCountry function after the subscription updates the selectedTaxonomy
-            countryNamesArray.forEach(array => {
-                colorCountry(array)
-            })
-            // colorCountry(countryNamesArray);
+        let countryNamesArray = countryCodesArray.map((country) => {
+            return matchCountryCodeWithCountryName(country, countryCodeToNamejson);
         });
 
-        return () => {
-            unsubscribe();
-        };
+        console.log("worst", countryNamesArray);
+
+        // Update the list of selected countries
+        countryNamesArray = countryNamesArray.flat();
+
+        // Call the colorCountry function after the subscription updates the selectedTaxonomy
+        colorCountry(countryNamesArray);
     });
 
-    function colorCountry(countryNames) {
-        d3.select("#content g.map").selectAll("path").style("fill", "green"); // Reset all countries to the original style
-        console.log('kaas+worst', countryNames)
+    return () => {
+        unsubscribe();
+    };
+  });
 
-        countryNames.forEach((countryName) => {
-            d3.select("#content g.map")
-                .selectAll("path")
-                .filter((d) => d.properties.name === countryName)
-                .style("fill", "orange");
-        });
-    }
+  function colorCountry(countryNames) {
+    // Reset all countries to green
+    d3.select("#content g.map")
+        .selectAll("path")
+        .style("fill", "green");
 
-    let matchSpecieWithCountryCode = (speciename, data) => {
+    // Apply the orange color to the selected countries
+    d3.select("#content g.map")
+        .selectAll("path")
+        .filter((d) => countryNames.includes(d.properties.name))
+        .style("fill", "orange");
+}
+
+  let matchSpecieWithCountryCode = (speciename, data) => {
     let countryCodes = [];
 
     data.forEach((datapoint) => {
-        if (datapoint.SPECIES_NAME_PRINT === speciename) {
-            countryCodes.push(datapoint.WCVP_WGSRPD_LEVEL_3_native);
-        }
+      if (datapoint.SPECIES_NAME_PRINT === speciename) {
+        countryCodes.push(datapoint.WCVP_WGSRPD_LEVEL_3_native);
+      }
     });
 
     console.log(countryCodes);
-    
+
     // Join the array elements into a string and remove square brackets
-    let joinedString = countryCodes.join(', ').replace(/[\[\]']+/g, "");
-    
+    let joinedString = countryCodes.join(", ").replace(/[\[\]']+/g, "");
+
     // Split the string into an array
     countryCodes = joinedString.split(", ");
-    
+
     console.log("kaas", countryCodes);
     return countryCodes;
-};
+  };
 
-
-let matchCountryCodeWithCountryName = (codes, data) => {
+  let matchCountryCodeWithCountryName = (codes, data) => {
     console.log("hoi", codes);
     const countryNames = [];
 
     codes.forEach((code) => {
-        data.forEach((datapoint) => {
-            if (datapoint.Code === code) {
-                countryNames.push(datapoint.WGSRPD_name);
-            }
-        });
+      data.forEach((datapoint) => {
+        if (datapoint.Code === code) {
+          countryNames.push(datapoint.WGSRPD_name);
+        }
+      });
     });
 
     console.log("kaas", countryNames);
     return countryNames;
-};
-
+  };
 </script>
 
 <div id="content">
-    <svg>
-        <g class="map"></g>
-    </svg>
+  <svg>
+    <g class="map"></g>
+  </svg>
 </div>
